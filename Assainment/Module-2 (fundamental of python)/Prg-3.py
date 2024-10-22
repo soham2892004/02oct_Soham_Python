1c1be752b63c6410407a7b192218c141decd0aea
sohamparmar <sohamparmar79@gmail.com>
2024-10-22 12:11:04 +0530

    initial commited

--- Assainment/Module-2 (fundamental of python)/Prg-3.py
+++ Assainment/Module-2 (fundamental of python)/Prg-3.py
@@ -1,17 +1,11 @@
-print("hello world")
-
 range_start = int(input("Enter the starting range: "))
 range_end = int(input("Enter the ending range: "))
 
-
 fib_series = [0, 1]
 
-
 for i in range(2, range_end + 1):
-  fib_series.append(fib_series[i - 1] + fib_series[i - 2])
-
+  fib_series.insert(i, fib_series[i - 1] + fib_series[i - 2])
 
 result = fib_series[range_start:range_end + 1]
-
 
 print("The Fibonacci series within the given range is:", result)
